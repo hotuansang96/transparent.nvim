72b9d22c387ebe3603e4e12324ee45890b09bccb
SangHT <hotuansang96@gmail.com>
2025-03-31 17:44:18 +0700

    add function

---
+++ lua/transparent/init.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+M.set_transparent = function(opts)
+  local prefixes = opts.prefixes or {}
+  for _, prefix in ipairs(prefixes) do
+    local highlights = vim.fn.getcompletion(prefix, "highlight")
+    for _, name in ipairs(highlights) do
+      local hl = vim.api.nvim_get_hl(0, { name = name, link = false })
+      if hl.bg then
+        hl.bg = "NONE" -- Preserve other attributes
+        vim.api.nvim_set_hl(0, name, hl)
+      end
+    end
+  end
+end
+
+return M
