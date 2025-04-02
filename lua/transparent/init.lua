1b0b3c8b0b31d1d5e2bfce7d38df83e14abf86bf
SangHT <hotuansang96@gmail.com>
2025-04-02 22:34:08 +0700

    update

--- lua/transparent/init.lua
+++ lua/transparent/init.lua
@@ -10,7 +10,7 @@ M.setup = function(opts)
         hl.bg = "NONE" -- Preserve other attributes
         vim.api.nvim_set_hl(0, name, hl)
       end
-      local command_string = "highlight " .. name .. " guibg=NONE"
+      local command_string = ":hi " .. name .. " guibg=NONE"
       vim.cmd(command_string)
     end
   end
